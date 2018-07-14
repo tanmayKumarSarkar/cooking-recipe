125317ce3998350368aa73560b11ae55b8d17a26
tanmaykumar1993 <tanmay.kr.sarkar@live.com>
2018-07-15 02:24:14 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'popper.js/dist/popper.min.js';
+import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import registerServiceWorker from './registerServiceWorker';
 import Router from './components/Router';
