207cf81bbac0b88a86596a5eefae0c9f1165a8c6
tanmaykumar1993 <tanmay.kr.sarkar@live.com>
2018-07-16 06:19:39 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import logo from '../logo.svg';
 const Header = props => (
     <header className="App-header">
     <div className="row">
-        <span className="col-3"><img src={logo} className="App-logo" alt="logo" /></span> 
-        <span className="col-5"><h1 className="App-title">Recipe Search</h1></span>
+        <span className="col-xs-3"><img src={logo} className="App-logo" alt="logo" /></span> 
+        <span className="col-xs-5"><h1 className="App-title">Recipe Search</h1></span>
     </div>
         
     </header>
